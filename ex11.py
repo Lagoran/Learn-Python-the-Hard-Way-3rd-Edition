c3346050c63a9d3dacc51fda1db9ed4fb0b653b3
John Doe <johndoe@example.com>
2025-10-04 16:43:55 +0300

    ex11 File exercises

---
+++ ex11.py
@@ -0,0 +1,11 @@
+print("How old are you ?!", end = '\n')
+age = input()
+
+print("You are %r years old." % (age))
+
+print(f"So you are {age} years old and that not too bad!")
+
+print("What is you favorite color?")
+color = input()
+print("So you favorite color is %s" % color)
+print(f"So you favorite color is {color} is it not?".format())
